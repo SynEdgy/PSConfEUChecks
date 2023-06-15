9a091c902453d2429913b8fa11c6f8914476185e
gaelcolas <gaelcolas@synedgy.com>
2023-06-15 21:52:44 +0200

    updating checks and going a bit faster

--- psconfeu2023.tests.ps1
+++ psconfeu2023.tests.ps1
@@ -1,5 +1,16 @@
 Describe 'Running PSConfEU 2023 Tests' {
 
+    Describe 'Location Checks' {
+        It 'Cubex Prague should be ready' {
+        }
+    }
+
+    Describe 'Code of Conduct Compliance' {
+        It 'All participants should have accepted the code of conduct' {
+            Start-Sleep -Milliseconds 700
+        }
+    }
+
     Describe 'There should be Sponsors' {
 
         $Sponsors = 'Chocolatey', 'Patch My PC', 'Script Runner', 'Nocentino Systems'
@@ -10,12 +21,16 @@ Describe 'Running PSConfEU 2023 Tests' {
         Context 'Most Amazing Sponsor <_>' -ForEach $Sponsors {
             It 'THANK YOU - <_>' {
             }
+
             It '<_> should have a logo' {
             }
+
             It 'There should be a stand for <_>' {
             }
+
             It 'All boxes for <_> shall arrive' {
             }
+            
             It 'We are MOST GRATEFUL for the support of <_> - THANK YOU' {
             }
         }
@@ -23,26 +38,22 @@ Describe 'Running PSConfEU 2023 Tests' {
 
     Context 'Catering Organisation checks' {
         It 'Should know the conference schedule' {
-            Start-Sleep -Seconds 2
-            $true | Should -Be $true
+            Start-Sleep -Milliseconds (Get-Random -Minimum 25 -Maximum 400)
         }
 
         It 'Should have coffee breaks ready' {
-            $true | Should -Be $true
         }
 
     }
 
     Context 'PowerShell Community Dinner checks' {
 
-        It 'Should have some content' {
-            Start-Sleep -Seconds 2
-            $true | Should -Be $true
+        It 'Should have the Restaurant ready' {
+            Start-Sleep -Milliseconds (Get-Random -Minimum 25 -Maximum 400)
         }
 
-        It 'Sponsors have some content' {
-            Start-Sleep -Seconds 1
-            $true | Should -Be $true
+        It 'Sponsors have some fun planned' {
+            Start-Sleep -Milliseconds (Get-Random -Minimum 25 -Maximum 400)
         }
     }
 
@@ -55,7 +66,7 @@ Describe 'Running PSConfEU 2023 Tests' {
             Context "<_.Name>" -ForEach $times {
                 $sessions = @($_.Group | Foreach-Object { @( $_."A1 (Track 1)", $_."A2 (Track 2)", $_."A3 (Track 3)", $_."B (Track 4)" | Where-Object { -not [string]::IsNullOrWhitespace($_)} | ForEach-Object { $_ -replace "\n", " - "})})
                 It "<_>" -ForEach $Sessions {
-                    Start-Sleep -Milliseconds (Get-Random -Minimum 100 -Maximum 500)
+                    Start-Sleep -Milliseconds (Get-Random -Minimum 25 -Maximum 400)
                 }
             }
         }
