708a76b60584104bf5a476e51fa847a2d32ba0ec
Jakub Jareš <me@jakubjares.com>
2023-06-15 09:29:25 +0200

    Add talks table

--- psconfeu2023.tests.ps1
+++ psconfeu2023.tests.ps1
@@ -5,7 +5,7 @@ Describe 'Running PSConfEU 2023 Tests' {
         It 'Should have some content' {
             $true | Should -be $true
         }
-        
+
         It 'Sponsors have some content' {
             $true | Should -be $true
         }
@@ -16,7 +16,7 @@ Describe 'Running PSConfEU 2023 Tests' {
             Start-Sleep -Seconds 2
             $true | Should -be $true
         }
-        
+
         It 'Should have coffee breaks ready' {
             $true | Should -be $true
         }
@@ -29,10 +29,60 @@ Describe 'Running PSConfEU 2023 Tests' {
             Start-Sleep -Seconds 2
             $true | Should -be $true
         }
-        
+
         It 'Sponsors have some content' {
             Start-Sleep -Seconds 1
             $true | Should -be $true
+        }
+    }
+
+    $agenda = @(
+        @{
+            Day = "Monday"
+            Sessions = @(
+                @{
+                    Time = "9:00"
+                    Titles = @(
+                        "Keynote"
+                    )
+                }
+                @{
+                    Time = "11:00"
+                    Titles = @(
+                        "Pester is the best"
+                        "Talk2"
+                    )
+                }
+            )
+        },
+        @{
+            Day = "Tuesday"
+            Sessions = @(
+                @{ 
+                    Time = "9:00"
+                    Titles = @(
+                        "talk 1"
+                        "Talk 2"
+                    )
+                }
+                @{
+                    Time = "10:00"
+                    Titles = @(
+                        "talk 1"
+                        "talk 2"
+                    )
+                }
+            )
+        }
+    )
+
+    Describe "Adding sessions" {
+        Describe "<Day>" -ForEach $Agenda {
+            Context "<_.Time>" -ForEach $_.Sessions {
+                It "<_>" -ForEach $_.Titles {
+                    Start-Sleep -Milliseconds (Get-Random -Minimum 100 -Maximum 500)
+                }
+            }
         }
     }
 }
